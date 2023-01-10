c7ee2b2d7f93c25ed96447accdac1a7ef1c3aeba
ahnyujin <ujahnn@gmail.com>
2023-01-11 02:37:25 +0900

    [CHANGE] Change to use snake case in sql

--- webFluxOnboarding/src/main/resources/schema-tasks-h2.sql
+++ webFluxOnboarding/src/main/resources/schema-tasks-h2.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS TASKS (
     id VARCHAR(100) NOT NULL,
     details VARCHAR(100) NOT NULL,
-    taskStatus VARCHAR(100) NOT NULL
+    task_status VARCHAR(100) NOT NULL
 );
 
-INSERT INTO TASKS(id, details, taskStatus)
+INSERT INTO TASKS(id, details, task_status)
 VALUES ('help', 'me', 'PLEASE');
