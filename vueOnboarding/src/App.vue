c0b241f36621224ebc24bcfe30805b8f7cda65e1
limyeonsoo <yslim6168@naver.com>
2023-01-12 00:34:12 +0900

    [ADD] Frontend Onboarding /w vue

---
+++ vueOnboarding/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <top-bar
+        :title="title"
+        :month="month"
+        :day="day"
+        :weekday="weekday"
+    ></top-bar>
+  </div>
+</template>
+
+<script>
+import TopBar from "./components/TopBar.vue";
+
+export default {
+  components: {
+    TopBar
+  },
+  data() {
+    return {
+      title: 'My Todo',
+      month: 9,
+      day: 13,
+      weekday: 'Thu'
+    }
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  width: 100%;
+}
+</style>
---
+++ vueOnboarding/src/components/TopBar.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="wrapper">
+    <div class="wrapper-left">
+      <div class="wrapper-left-menu">
+        <img src="./icons/ic_topbar_menu.svg" alt="menu">
+      </div>
+      <div class="wrapper-left-title">
+        {{ title }}
+      </div>
+    </div>
+    <div class="wrapper-right">
+      {{ today }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    month: {
+      type: Number,
+      require: true
+    },
+    day: {
+      type: Number,
+      required: true
+    },
+    weekday: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    today() {
+      return `${this.month}/${this.day} ${this.weekday}`
+    }
+  }
+}
+</script>
+
+<style scoped>
+.wrapper {
+  position: absolute;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 48px;
+  padding: 0 16px;
+  background-color: #6C77A7;
+}
+.wrapper-left {
+  width: 128px;
+  display: flex;
+  align-items: center;
+}
+.wrapper-left-menu {
+  width: 24px;
+  height: 24px;
+}
+.wrapper-left-title {
+  padding-left: 14px;
+  font-size: 24px;
+  line-height: 24px;
+  font-weight: 700;
+  color: #FFFFFF;
+  white-space: nowrap;
+}
+
+.wrapper-right {
+  width: 65px;
+  height: 19px;
+  line-height: 19px;
+  font-size: 16px;
+  font-weight: 700;
+  color: #FFFFFF;
+}
+</style>
